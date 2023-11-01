9e6b0ad3a7896bd02e68f8d40c0bcc2296644330
RaynisonTD <raynison.daal@student.hu.nl>
2023-11-01 16:33:35 +0100

    moderation system code werkt nu
    De moderator  code skipt nu over de berihcten heen die all gemoderate zijn

--- Module_2.py
+++ Module_2.py
@@ -1,3 +1,4 @@
+
 import csv
 from getpass import getpass
 from datetime import datetime
@@ -5,33 +6,98 @@ from datetime import datetime
 
 def moderatorAunthentificeren():
     email = input('voer je moderator email in: ')
-    wachtwoord = getpass('voer je wachtwoord in: ')
+    naam = input('voer je naam in : ')
 
     # check of de moderator de juiste gegevens invoerd
-    if email == 'moderator@mail.com' and wachtwoord == '12345678':
-        return True
+    if '@' in email and naam != '':
+        return naam
+
     else:
-        return False
+         return None
 
 
 # moderatie van de berichten
 def moderatie():
     # als de moderator is ingelogd
-    if moderatorAunthentificeren():
-        csv_file = 'gebruiker_info.csv'
+    moderator_naam = moderatorAunthentificeren()
 
-        # lees het csv bestand in en ga de berichten na
-        berichtLijst = []
-        with open(csv_file, mode='r', newline='') as file:
+
+    if moderator_naam:
+        csv_file = 'berichten.csv'
+
+
+        # creer een tijdelijk lijst met alle berichten
+        gekeurde_berichten = []
+
+        with open(csv_file, mode='r+', newline='') as file:
             # maak van elke regel in het csv bestand een dictionary
-            reader = csv.DictReader()
-            # itereer door de dictionary van berichten en voeg deze toe aan een niewue lijst
-            for rij in csv.DictReader():
-                berichtLijst.append(rij)
+            reader = csv.DictReader(file)
+            fieldnames = reader.fieldnames
 
-        moderatorNaam = input('moderator naam: ')
-        moderatorEmail = input('moderator email: ')
 
-        # maak de goedkeuringstabel(dit gebeurt maar een keer)
+            # laat het bericht zien voor goedkeuring
+            for i, rij in enumerate(reader):
 
-        # laat het bericht zien voor goedkeuring
+                if rij.get('goedgekeurd?') and rij.get("gekeurd door:"):
+
+                    gekeurde_berichten.append(rij)
+                    continue
+
+                else:
+
+                    print(f"bericht {i + 1}:")
+                    print(f"naam: {rij['naam']}")
+                    print(f"bericht: {rij['bericht']}")
+                    print("opties:")
+                    print("2. afkeuren")
+                    print("1. goedkeuren")
+
+                keuze = input('word dit berichten goedgekeurd (j) of afgekeurd (n)? (j/n): ')
+                goedgekeurd = "Ja" if keuze == "j" else "Nee"
+
+                #voeg de goedkeuring en de moderator naam toe aan de tabel
+                if goedgekeurd == 'Ja' or goedgekeurd =='Nee':
+                    rij["goedgekeurd?"] = goedgekeurd
+                    rij['gekeurd door:'] = moderator_naam
+
+
+                gekeurde_berichten.append(rij)
+
+
+            # Reopen the file and write the modified rows
+            with open(csv_file, mode='w', newline='') as write_file:
+                writer = csv.DictWriter(write_file, fieldnames=fieldnames)
+                writer.writeheader()
+                writer.writerows(gekeurde_berichten)
+
+
+
+
+
+
+
+        print('bestanden zijn bijgewerkt in {csv_bestand}')
+
+    else:
+        print("authentifiatie is gefaald")
+
+if __name__ == "__main__":
+    moderatie()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
